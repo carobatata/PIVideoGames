23d448566c70d9f76b15c6c123ee48ebe38399f7
carobatata <aacostacarolina@gmail.com>
2021-10-29 20:00:25 +0200

    new AddVideogame

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const videogamesRoute = require('./videogames');
 const videogameRoute = require('./videogame');
 const genresRoute = require('./genres');
+const platformsRoute = require('./platforms');
 
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/videogames', videogamesRoute);
 router.use('/videogame', videogameRoute);
 router.use('/genres', genresRoute);
+router.use('/platforms', platformsRoute);
 
 
 module.exports = router;
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -5,8 +5,51 @@ const axios = require('axios');
 const { APIKEY } = process.env;
 const {Op} = require('sequelize');
 
+async function getAllVideogamesFromApi() {
+    var promises = [
+        axios.get(`https://api.rawg.io/api/games?key=${APIKEY}`),
+        axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=2`),
+        axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=3`),
+        axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=4`),
+        axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=5`),
+    ]
+
+    let videogamePromiseApi = await Promise.all(promises);
+    videogamePromiseApi = videogamePromiseApi.map((v) => v.data.results)
+    videogamePromiseApi = videogamePromiseApi.flat().map((v) => {
+        return {
+            id: v.id,
+            name: v.name,
+            description: v.description,
+            image: v.background_image,
+            releaseDate: v.released,
+            rating: v.rating,
+            platforms: v.platforms.map(p => p.platform.name),
+            genres: v.genres.map(g => g.name)
+        }
+    })
+    // console.log(videogamePromiseApi.map(v => v.platforms).flat().filter((value, index, self) => self.indexOf(value) === index))
+    return videogamePromiseApi;
+}
+
+async function getAllVideogames() {
+    let videogamePromiseDb = await Videogame.findAll({
+        include: [{
+            model: Genre,
+            attributes: ['name'],
+            through: {
+                attributes: []
+            }
+        }]
+    })
+    let videogamePromiseApi = await getAllVideogamesFromApi()
+
+    return [...videogamePromiseApi, ...videogamePromiseDb];
+}
+
 router.get('/', async (req, res, next) => {
-    const { name } = req.query;
+    const { name, genre } = req.query;
+
     let videogamePromiseApi;
     let videogamePromiseDb;
     let allVideogames;
@@ -35,30 +78,26 @@ router.get('/', async (req, res, next) => {
 
             allVideogames = [...videogamePromiseDb, ...videogamePromiseApi];
             allVideogames = allVideogames.slice(0, 15);
-    
-        } else {
-            
-            videogamePromiseDb = await Videogame.findAll({
+        
+        }
+         else if(genre) {
+
+            videogamePromiseApi = await getAllVideogamesFromApi()
+
+            videogamePromiseApi = videogamePromiseApi.filter((v) => v.genres.includes(genre))
+
+            videogamePromiseDb =  await Videogame.findAll({
                 include: [{
                     model: Genre,
+                    where: { name: genre },
                     attributes: ['name'],
                     through: {
                         attributes: []
                     }
                 }]
-            })
+            });
 
-            var promises = [
-                axios.get(`https://api.rawg.io/api/games?key=${APIKEY}`),
-                axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=2`),
-                axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=3`),
-                axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=4`),
-                axios.get(`https://api.rawg.io/api/games?key=${APIKEY}&page=5`),
-            ]
-    
-            videogamePromiseApi = await Promise.all(promises);
-            videogamePromiseApi = videogamePromiseApi.map((v) => v.data.results)
-            videogamePromiseApi = videogamePromiseApi.flat().map((v) => {
+            videogamePromiseDb = videogamePromiseDb.map((v) => {
                 return {
                     id: v.id,
                     name: v.name,
@@ -67,10 +106,18 @@ router.get('/', async (req, res, next) => {
                     releaseDate: v.released,
                     rating: v.rating,
                     platforms: v.platforms.map(p => p.platform.name),
-                    genres: v.genres.map(g => g.name)
-                }
-            })
-            allVideogames = [...videogamePromiseApi, ...videogamePromiseDb];
+                    genres: v.genres.map(g => g.name),
+                    createdInDb: v.createdInDb
+                }})
+
+            allVideogames = [...videogamePromiseDb, ...videogamePromiseApi];
+          
+         }
+    
+        else {
+            
+            allVideogames = await getAllVideogames();
+           
         }
             res.send(allVideogames);
        
@@ -80,6 +127,8 @@ router.get('/', async (req, res, next) => {
         next(error);
     }   
 })
+
+
 
 
 module.exports = router;
