1b9d151543c439333f63608edb6ea4cf2018f7ad
carobatata <aacostacarolina@gmail.com>
2021-11-03 09:38:46 +0100

    Api tests

--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -2,7 +2,7 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Videogame, conn } = require('../../src/db.js');
+const { Videogame, Genre, conn } = require('../../src/db.js');
 
 const agent = session(app);
 
@@ -11,7 +11,8 @@ describe('Videogame routes', () => {
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => conn.sync({ force: true }));
+  beforeEach(() => conn.sync({ force: true })
+  .then(() => Genre.create({ name: 'Action'})));
   describe('POST /videogame', () => {
     it('should get 200', () =>
         agent.post('/videogame').send({
@@ -40,60 +41,27 @@ describe('Videogame routes', () => {
         expect(videogame).to.exist;
       })
     })
+    it('sets correctly the Genre in database', function() {
+      return agent.post('/videogame')
+      .send ({
+        name: 'Super Mario Bross',
+        description: 'abc',
+        platforms: ['Play Station'],
+        genres: ['Action', 'Adventure']
+      })
+      .then(() => {
+        return Videogame.findOne({
+          where: {
+            name: 'Super Mario Bross'
+          },
+          include: {
+            model: Genre
+          }
+        })
+      })
+      .then(videogame => {
+        expect(videogame.genres[0].name).to.equal('Action')
+      })
+    })
   });
 });
-
-
-
-//   it('crea una page en la base de datos', function(){
-//     return agent.post('/pages')
-//       .send({
-//         title: 'hola',
-//         content: 'chau',
-//         authorEmail: 'toni@toni.com',
-//         authorName: 'Franco',
-//         categories: [1]
-//       })
-//       .then(() => {
-//         return Page.findOne({
-//           where: {
-//             title: 'hola'
-//           }
-//         });
-//       })
-//       .then(page => {
-//         expect(page).to.exist;
-//       });
-//   });
-//   it('setea correctamente la categoría en la base de datos', function(){
-//     return agent.post('/pages')
-//       .send({
-//         title: 'hola',
-//         content: 'chau',
-//         authorEmail: 'toni@toni.com',
-//         authorName: 'Franco',
-//         categories: [1,2]
-//       })
-//       .then(() => {
-//         return Page.findOne({
-//           where: {
-//             title: 'hola'
-//           },
-//           include: {
-//             model: Category
-//           }
-//         });
-//       })
-//       .then(page => {
-//         expect(page.categories[0].name).to.equal('Autos');
-//         expect(page.categories[1].name).to.equal('Deportes');
-//       });
-//   });
-// });
-
-// });
-
-// describe('pedidos http USER', function () {
-// beforeEach(function(){
-//   return User.sync({ force: true })
-// })
