78bf6bc7e1a6e0da5609f16e664b6e1eb1aaaa04
carobatata <aacostacarolina@gmail.com>
2021-11-19 14:05:19 +0100

    removing dev tools for react

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,13 +1,11 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "../reducer/index";
 import thunk from "redux-thunk";
 
 
 const store = createStore(
     reducer,
-    compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    applyMiddleware(thunk)
   );
 
 export default store;
